cd96d9a4415157221460f02620e298413d21673c
Tuncay Basak <tuncay.basak@outlook.com>
2020-10-16 14:36:22 +0200

    Test localeo-api

--- App.js
+++ App.js
@@ -2,7 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Api from 'localeo-api';
+
+async function test()
+{
+  console.log(await Api.login('tuncay', '12345678'));
+  console.log(await Api.createMessage({ destinaterId: 2, text: "Salut Olivier!" }));
+}
+
 export default function App() {
+  test();
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
